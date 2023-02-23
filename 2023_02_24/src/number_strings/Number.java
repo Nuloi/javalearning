5f23d5edd725cacca0db0f8e3dedbe1119c3519c
Nuloi <ekgus8056@gmail.com>
2023-02-24 00:28:09 +0900

    0224

---
+++ 2023_02_24/src/number_strings/Number.java
@@ -0,0 +1,10 @@
+package number_strings;
+
+public class Number {
+
+	public static void main(String[] args) {
+		System.out.println(1+2);
+
+	}
+
+}
---
+++ 2023_02_24/src/number_strings/String.java
@@ -0,0 +1,13 @@
+package number_strings;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		System.out.println("string");
+		//System.out.println('string');<-- ''은 문자취급 ""은 문자열 취급
+		System.out.println("1"+"1");//<-- 문자열은 더하면 문자열이 합쳐저서 문자의 연산 출력
+		System.out.println("\"egoing said\" Welcome programming world");//문자해석을 강제로 하는것을 escape(이스케이프), 하고함 \<- 사용
+		System.out.println("HTML/nCSS/nJave\n");//C언어와 같이 서식문자로 출력이랑 같음
+	}
+
+}
