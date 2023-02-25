3f6926fb976cab5778a596d275a2e1e860dd6196
Nuloi <ekgus8056@gmail.com>
2023-02-25 22:33:16 +0900

    0225

---
+++ 2023_02_25/src/Variable/a01.java
@@ -0,0 +1,21 @@
+package Variable;
+
+public class a01 {
+
+	public static void main(String[] args) {
+		int a = 1;
+		System.out.println(a+1);
+		a = 2;
+		System.out.println(a+1);
+		
+		
+		double b;
+		b = 1.2 ;
+		System.out.println(b+0.1);
+		
+		String c;
+		c = "coding";
+		System.out.println(c);
+	}
+
+}
