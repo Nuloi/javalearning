d9eede73008e48a9ee77065330c8201291accd7d
Nuloi <ekgus8056@gmail.com>
2023-02-27 23:34:45 +0900

    02_27

---
+++ 2023_02_27/src/a/IF.java
@@ -0,0 +1,17 @@
+package a;
+
+public class IF {
+
+	public static void main(String[] args) {
+		String dd = args[0];
+		int a= 2;
+		if(dd.equals("bam")) {
+			if(a==2) {
+				System.out.print("a==2");
+			}
+		}else {
+			System.out.print("dd/=bam");
+		}
+	}
+
+}
